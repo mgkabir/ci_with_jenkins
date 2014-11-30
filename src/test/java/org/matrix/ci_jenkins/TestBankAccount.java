683875d3062e2874c9a100f26aff0ce7ef2cac4b
Kabir <mgkabir@gmail.com>
2014-11-30 20:27:07 +0600

    one more Test method added for InsufficientFunds.

--- src/test/java/org/matrix/ci_jenkins/TestBankAccount.java
+++ src/test/java/org/matrix/ci_jenkins/TestBankAccount.java
@@ -7,13 +7,23 @@ import org.junit.Test;
 /* Test class for BankAccount */
 
 public class TestBankAccount {
- 
+
 	@Test
-	public void testDebitWithSufficientFunds(){
+	public void testDebitWithSufficientFunds() {
 		BankAccount account = new BankAccount(10);
-		
+
 		double amount = account.debit(5);
-		
+
 		Assert.assertEquals(5.0, amount);
 	}
-} 
+	
+	@Test
+	public void testDebitWithInsufficientFunds() {
+		BankAccount account = new BankAccount(10);
+
+		double amount = account.debit(11);
+
+		Assert.assertEquals(10.0, amount);
+	}
+	
+}
