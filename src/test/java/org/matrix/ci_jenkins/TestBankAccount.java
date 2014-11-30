422e26a5a88f2e386ef1a40cd79bf1810acb19be
Kabir <mgkabir@gmail.com>
2014-11-30 19:54:53 +0600

    added jacoco plugin and a test

--- src/test/java/org/matrix/ci_jenkins/TestBankAccount.java
+++ src/test/java/org/matrix/ci_jenkins/TestBankAccount.java
@@ -2,10 +2,18 @@ package org.matrix.ci_jenkins;
 
 import junit.framework.Assert;
 import org.junit.Before;
-import org.junit.Test
+import org.junit.Test;
 
 /* Test class for BankAccount */
 
 public class TestBankAccount {
  
+	@Test
+	public void testDebitWithSufficientFunds(){
+		BankAccount account = new BankAccount(10);
+		
+		double amount = account.debit(5);
+		
+		Assert.assertEquals(5.0, amount);
+	}
 } 
