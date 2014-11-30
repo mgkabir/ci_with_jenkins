05993e2bdd3c83fd4bdc79b89f27d466f55b7b69
Kabir <mgkabir@gmail.com>
2014-11-30 19:24:35 +0600

    Testing Webhook, modified TTestBankAccount class

--- src/test/java/org/matrix/ci_jenkins/TestBankAccount.java
+++ src/test/java/org/matrix/ci_jenkins/TestBankAccount.java
@@ -3,7 +3,9 @@ package org.matrix.ci_jenkins;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
- 
+
+/* Test class for BankAccount */
+
 public class TestBankAccount {
  
 } 
